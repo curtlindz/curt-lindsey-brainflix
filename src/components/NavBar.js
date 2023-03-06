b1ece163c08bc3b9cbad89f22b09aa0187b8605b
Curtis Lindsey <curtislindsey@Curtiss-MacBook-Pro.local>
2023-03-05 19:15:55 -0500

    trying to remove node_modules

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import '../components/NavBar.scss';
 import { Link } from "react-router-dom";
 
 
-export default function NavBar() {
+export default function NavBar() { 
     return (
         <div className="navbar">
             <Link to ={'/'}>
